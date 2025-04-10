3c6811c2831e63cf0670c8916b94248372b2980a
Jana <janaAli12>
2025-04-10 08:48:17 -0700

    database connection

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { AppService } from './app.service';
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  const appService = app.get(AppService);
+  console.log('Mongo URI:', appService.getMongoUri());
+  console.log('Database Name:', appService.getDatabaseName());
+  app.enableCors({
+    origin: 'http://localhost:3001',  // Ensure this is the correct frontend URL
+    methods: ['GET', 'POST'],
+  });
+
+ // await app.listen(3000);
+  console.log('Application is running on http://localhost:3000');
 }
 bootstrap();
