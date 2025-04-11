ca8a24eb613bf94ce8e9757ecb300b085e7aff5c
habiba58t <habibatalha2004@gmail.com>
2025-04-11 16:31:22 +0300

    User schema

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserControllerController } from './user-controller/user-controller.controller';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule],
+  controllers: [AppController, UserControllerController, UserController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
